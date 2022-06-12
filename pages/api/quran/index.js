7da6af19d8682974ae2a4e29d260b96d48dc5636
maru-yasa <marufilyasa13@gmail.com>
2022-06-12 14:39:13 +0700

    make eror handler

--- pages/api/quran/index.js
+++ pages/api/quran/index.js
@@ -1,7 +1,14 @@
 import fs from 'fs'
 
 export default function handler(req, res) {
-    var quran = JSON.parse(fs.readFileSync('data/quran.json', 'utf8'));
-    res.status(200).json(quran)
+    try {
+        var quran = JSON.parse(fs.readFileSync('data/quran.json', 'utf8'));
+        res.status(200).json(quran)
+    } catch (error) {
+        res.status(200).json({
+            error: error
+        })
+    }
+
 }
   
